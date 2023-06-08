ff7a3aebbb3a61e4708cc2ec1269aed3644f2baf
Carlos Canales <nicatronico@gmail.com>
2023-06-08 14:59:04 -0600

    Actualización del header

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,3 +19,16 @@ export default {
 
     plugins: [forms],
 };
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
